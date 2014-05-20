527f8d2a5cc696290550e7467e8f1e06f9e797b8
Krzysztof Romanowski <romanowski.kr@gmail.com>
2014-05-20 16:42:16 +0200

    init commit

---
+++ Min.scala
@@ -0,0 +1,43 @@
+import scala.reflect.runtime._
+import scala.tools.reflect.ToolBox
+
+object Min extends App {
+
+  import universe._
+
+
+  lazy val toolbox: ToolBox[universe.type] = universe.runtimeMirror(getClass.getClassLoader).mkToolBox()
+
+  val code = "List(1,2).mkString"
+  val parsed = toolbox.parse(code)
+  val typechecked = toolbox.typecheck(parsed)
+
+
+  val Select(listTree, name) = typechecked
+  val Apply(listApplyTree, params) = listTree
+  val newArg = toolbox.parse("Ala(a)")
+  val Apply(alaApply, _) = newArg
+  val newParams = params.map(value => Apply(alaApply, List(value)))
+
+  val classCode = toolbox.parse("case class Ala(a: Int)")
+
+  val newBody = Block(classCode,Select(Apply(listApplyTree, newArg :: newParams), name))
+
+
+
+  // gets AST of function arguments required in expression
+  val functionArgs: List[ValDef] = {
+    val dummyFunction = s"""(a: Int) => ???"""
+    val parsed = toolbox.parse(dummyFunction)
+    val Function(args, _) = toolbox.typecheck(parsed)
+    args
+  }
+
+  //modification of AST
+  val newTree = Function(functionArgs, newBody)
+
+  val reseted = toolbox.untypecheck(newTree)
+  println(reseted)
+  val compiled = toolbox.compile(reseted)
+  println(compiled())
+}
